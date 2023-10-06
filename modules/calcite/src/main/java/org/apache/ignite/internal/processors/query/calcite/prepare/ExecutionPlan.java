4e1fb7741f8e11bda4c3212df47ac35ad10d3aa2
Mark Dodds <marktdodds@gmail.com>
2023-10-05 21:05:01 -0400

    Debugging

--- modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/prepare/ExecutionPlan.java
+++ modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/prepare/ExecutionPlan.java
@@ -219,7 +219,7 @@ class ExecutionPlan {
         }
 
 //        fragments = ImmutableList.copyOf(newFragments);
-        return new ExecutionPlan(ver, fragments().stream().map(f -> f.remap(f.mapping())).collect(Collectors.toList()));
+        return new ExecutionPlan(ver, newFragments);
 //        return this;
     }
 
