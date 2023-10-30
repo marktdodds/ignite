88b635320cd5b3119ad8aeea80b0c34637548365
Mark Dodds <marktdodds@gmail.com>
2023-10-30 16:46:21 -0400

    Testing distributed nested loop join

--- modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/rel/IgniteDistributedNestedLoopJoin.java
+++ modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/rel/IgniteDistributedNestedLoopJoin.java
@@ -86,7 +86,7 @@ public class IgniteDistributedNestedLoopJoin extends AbstractIgniteJoin {
     public RelOptCost computeSelfCost(RelOptPlanner planner, RelMetadataQuery mq) {
         IgniteCostFactory costFactory = (IgniteCostFactory) planner.getCostFactory();
 
-        if (!"true".equalsIgnoreCase(System.getenv("MD_FORCE_DJ"))) return costFactory.makeTinyCost();
+        if ("true".equalsIgnoreCase(System.getenv("MD_FORCE_DJ"))) return costFactory.makeTinyCost();
         if (!"true".equalsIgnoreCase(System.getenv("MD_USE_ENHANCEMENTS"))) return costFactory.makeInfiniteCost();
 
         double leftCount = mq.getRowCount(getLeft());
